05cd3e2f18d565edceb219275a036f76e46c028f
mr_neither <mr.neitherman@gmail.com>
2018-11-10 09:21:15 +0300

    Worked client and server version without CNN

--- Client/user_service.py
+++ Client/user_service.py
@@ -1,36 +1,179 @@
-import socket
+from cryptography.fernet import Fernet
+import socket, cv2, json, numpy as np
+import Crypto.PublicKey.RSA as RSA
+
+
+def encryption_sender_decorator_maker(key):
+    cipher = Fernet(key)
+
+    def new_decorator(sender, recver):
+        def ecnrypt_and_send(open_text: str):
+            if isinstance(open_text, str):
+                open_text = open_text.encode()
+            encrypted_text = cipher.encrypt(open_text)
+            return sender(encrypted_text)
+
+        def recv_and_decrypt(buff_size: int):
+            cypher_text = recver(buff_size)
+            return cipher.decrypt(cypher_text)
+
+        return ecnrypt_and_send, recv_and_decrypt
+
+    return new_decorator
+
+
+class ClientRequester:
+    @staticmethod
+    def registration_request(user_id, username, data_size: int, shape: list):
+        return f'{{"id" : {user_id}, ' \
+               f'"type": "Registration", ' \
+               f'"name": "{username}" , ' \
+               f'"data_size" : {ClientRequester.load_data_size(user_id, data_size)} , ' \
+               f'"shape" : {shape} }}'
+
+    @staticmethod
+    def authentication_request(user_id, username, data_size: int, shape: list):
+        return f'{{"id" : {user_id}, ' \
+               f'"type": "Authentication", ' \
+               f'"name": "{username}" , ' \
+               f'"data_size" : {ClientRequester.load_data_size(user_id, data_size)} , ' \
+               f'"shape" : {shape} }}'
+
+    @staticmethod
+    def update_request(user_id, username, data_size: int, shape: list):
+        return f'{{"id" : {user_id}, ' \
+               f'"type" : "Update" , ' \
+               f'"name" : "{username}" , ' \
+               f'"data_size" : {ClientRequester.load_data_size(user_id, data_size)} , ' \
+               f'"shape" : {shape} }}'
+
+    @staticmethod
+    def delete_request(user_id, username):
+        return f'{{"id" : {user_id}, "type" : "Delete" ,"name" : "{username}" }}'
+
+    @staticmethod
+    def load_data(user_id, data: bytes):
+        # return f'{{"id" : {user_id}, "type" : "Load" ,"data" : "{data}" }}'
+        return data
+
+    @staticmethod
+    def load_data_size(user_id, data_size: int):
+        # return len(f'{{"id" : {user_id}, "data" : }}') + data_size + 50
+        n = data_size + 1
+        return 100 + (n//16)*20 + (n//48)*4
+
+    @staticmethod
+    def hello_ack_request(user_id: int, key: bytes):
+        return f'{{"id" : {user_id}, ' \
+               f'"type" : "HelloACK" , ' \
+               f'"session_key" : "{key.decode()}" }}'
 
 
 class Client:
 
     sock = None
+    RavelPhotoFrame = None
+    Shape = None
+    user_id = 0
+
     Answer = None
 
     def __init__(self):
         self.sock = socket.socket()
 
-    def connect(self, ip='127.0.0.1', port=2018):
+    def start_session(self, ip='127.0.0.1', port=2019):
         self.sock.connect((ip, port))
+        self.send = self.sock.send
+        self.recv = self.sock.recv
 
-    def authentication(self, username, bio_parameter):
-        request = f'{{ "type": "Authentication", "name": "{username}" , "biometric" : "{bio_parameter}" }}'
-        self.sock.send(request.encode())
-        self.Answer = self.sock.recv(1024)
+        ans = self.recv(1024).decode()
+        print(ans)
+        answer = json.loads(ans)
 
-    def registration(self, username, bio_parameter):
-        request = f'{{ "type": "Registration", "name": "{username}" , "biometric" : "{bio_parameter}" }}'
-        self.sock.send(request.encode())
-        self.Answer = self.sock.recv(1024)
+        self.user_id = answer['Your new id']
+        print(self.user_id)
 
-    def change_bio_parameter(self, username, new_bio_parameter):
-        request = f'{{ "type" : "Update" , "name" : "{username}" , "new_biometric" : "{new_bio_parameter}" }}'
-        self.sock.send(request.encode())
-        self.Answer = self.sock.recv(1024)
+        # print(answer['public_key'])
+        session_key = Fernet.generate_key()
+
+        self.send(ClientRequester.hello_ack_request(self.user_id, session_key).encode())
+
+        decorator = encryption_sender_decorator_maker(session_key)
+        self.send, self.recv = decorator(self.send, self.recv)
+
+    def authentication(self, username, faceframe =None):
+        if faceframe:
+            self.Shape = faceframe.shape
+            self.RavelPhotoFrame = faceframe.ravel()
+        else:
+            self.get_face()
+        # Todo size input
+        request = ClientRequester.authentication_request(
+            self.user_id, username, len(self.RavelPhotoFrame), list(self.Shape))
+
+        self.send(request)
+        while not self.load(self.RavelPhotoFrame):
+            pass
+        self.Answer = self.recv(1024)
+
+    def registration(self, username, faceframe=None):
+
+        if faceframe:
+            self.Shape = faceframe.shape
+            self.RavelPhotoFrame = faceframe.ravel()
+        else:
+            self.get_face()
+
+        request = ClientRequester.registration_request(self.user_id, username, len(self.RavelPhotoFrame), list(self.Shape))
+        self.send(request)
+        while not self.load(self.RavelPhotoFrame):
+            pass
+        self.Answer = self.recv(1024)
+
+    def load(self, data):
+        request = ClientRequester.load_data(self.user_id, bytes(data))
+
+        self.send(request)
+        answer = self.recv(1024)
+        return True
+        # Todo check to good load part
+
+    def change_bio_parameter(self, username, new_faceframe=None):
+        if new_faceframe:
+            self.Shape = new_faceframe.shape
+            self.RavelPhotoFrame = new_faceframe.ravel()
+        else:
+            self.get_face()
+        request = ClientRequester.update_request(self.user_id, username, len(self.RavelPhotoFrame), list(self.Shape))
+        self.send(request)
+        while not self.load(self.RavelPhotoFrame):
+            pass
+        self.Answer = self.recv(1024)
+        # Todo check server answer for ACK download
 
     def delete_user(self, username):
-        request = f'{{ "type" : "Delete" ,"name" : "{username}" }}'
-        self.sock.send(request.encode())
-        self.Answer = self.sock.recv(1024)
+        request = ClientRequester.delete_request(self.user_id, username)
+        self.send(request)
+        self.Answer = self.recv(1024)
 
     def get_answer(self):
         return self.Answer
+
+    def get_face(self):
+        cap = cv2.VideoCapture(0)
+        for i in range(15):
+            cap.read()
+
+        ret, frame = cap.read()
+        while not self.good_photo(frame):
+            ret, frame = cap.read()
+        print(frame)
+        self.Shape = frame.shape
+        self.RavelPhotoFrame = frame.ravel()
+        cap.release()
+
+    def good_photo(self, frame):
+        return True
+
+    def close(self):
+        self.sock.close()
--- Server/server_manager.py
+++ Server/server_manager.py
@@ -1,96 +1,209 @@
 from Server.Database_manager import DBManager
-import json
-
+from cryptography.fernet import Fernet
+import json, socket, random, numpy as np
+import Crypto.PublicKey.RSA as RSA
 
 # Todo Add processing of new types requests
-'''
-Types request (NEED):
-{type: 'Registration', name: NAME, parts : NUM OF PARTS}
-{type: 'Authentication', name: NAME, parts : NUM OF PARTS}
-{type: 'Data', part: PART, data: DATA}
-{type: 'Delete' , name: NAME}
-{type: 'Update' , name : NAME , biometric: BIOMETRIC}
+# Todo problem with specifying the size of the buffer to load the file. When encrypted, it increases
+# bug for some errors the server falls. To make the error handler
 
 
-Types request (NOW):
-{type: 'Registration', name: NAME, biometric : BIOMETRIC}
-{type: 'Authentication', name: NAME, parts : BIOMETRIC}
-{type: 'Delete' , name: NAME}
-{type: 'Update' , name : NAME , biometric: BIOMETRIC}
-'''
+def encryption_sender_decorator_maker(key):
+    cipher = Fernet(key)
+
+    def new_decorator(sender, recver):
+        def ecnrypt_and_send(open_text):
+            if isinstance(open_text, str):
+                open_text = open_text.encode()
+            encrypted_text = cipher.encrypt(open_text)
+            return sender(encrypted_text)
+
+        def recv_and_decrypt(buff_size: int):
+            cypher_text = recver(buff_size)
+            return cipher.decrypt(cypher_text)
+
+        return ecnrypt_and_send, recv_and_decrypt
+
+    return new_decorator
+
+
+class ServerResponder:
+    @staticmethod
+    def hello(data: str, new_id: int):
+        return f'{{"type" : "Hello" , "Your new id" : "{new_id}" , "public_key" : "{data.encode()}" }}'.encode()
+
+    @staticmethod
+    def error(info: str):
+        return ServerResponder.respond_template("Error", info)
+
+    @staticmethod
+    def success(info: str):
+        return ServerResponder.respond_template("Success", info)
+
+    @staticmethod
+    def respond_template(type_r, info):
+        return f'{{ "type" : "{type_r}" , "info" : "{info}" }}'
 
 
 class Session:
-
-    User = None
+    user_id = None
+    Username = None
     AuthSuccess = None
     Answer = None
     dbManager = None
 
-    def __init__(self, user=None, bio_parameter=None):
+    send = recv = lambda: None
+
+    # Review Need this fields?
+    PredictPhoto = None
+    public_key = None
+    private_key = None
+
+    def __init__(self, classifier):
         self.AuthSuccess = False
         self.dbManager = DBManager()
-        if bio_parameter and user:
-            self.authentication(user, bio_parameter)
+        self.public_key = open('PUBLIC.pem').read()
+        self.private_key = open('PRIVATE.bin').read()
+        self.classifier = classifier
 
-    def registration(self, username, bio_parameter):
-        if not (username and bio_parameter):
-            self.Answer = b'{ "type" : "Error" , "info" : "Incorrect name or biometric parameters" }'
+    def start_session(self, conn: socket.socket, addr: tuple):
+        try:
+            self.user_id = random.randint(10, 100)
+            self.send = conn.send
+            self.recv = conn.recv
+
+            self.send(
+                ServerResponder.hello(
+                    self.public_key, self.user_id))
+
+            request = self.recv(1024).decode()
+            print(request)
+            self.processing_request(request)
+            self.work()
+        except Exception as e:
+            print(e)
+            raise e
+
+    def work(self):
+        try:
+            while True:
+                data = self.recv(1024)
+                print(data)
+                self.processing_request(data)
+                self.send(self.get_answer())
+        except Exception as e:
+            print(e)
+            raise e
+
+    def registration(self, username: str):
+
+        if not (username and self.check_photo()):
+            self.Answer = ServerResponder.error("Incorrect name or Photo")
             return
+
         if self.dbManager.exist(username):
-            self.Answer = b'{ "type" : "Error", "info" : "User with this name already exists" }'
+            self.Answer = ServerResponder.error("User with this name already exists")
             return
-        self.User = username
-        self.dbManager.add_user(username, bio_parameter)
-        self.Answer = b'{ "type" : "Success" , "info" : "Registration success"}'
 
-    def authentication(self, username, bio_parameter):
+        self.Username = username
+        # Done self.PredictPhoto must contains vecotr<256> after calling CNN-method
+        self.dbManager.add_user(username, self.PredictPhoto)
+        self.Answer = ServerResponder.success("Registration success")
+
+    def check_photo(self):
+        return True
+
+    def authentication(self, username: str):
         self.AuthSuccess = False
-        if self.dbManager.exist(username):
-            # Todo if biometric.auth(bio_parameter,self.dbManager.get_user(name)) :
-            if self.dbManager.get_user(username)[1] == bio_parameter:
-                self.AuthSuccess = True
-                self.User = username
-                self.Answer = b'{ "type" : "Success" , "info" : "Auth success" }'
-            else:
-                self.Answer = b'{ "type" : "Error" , "info" : "Auth bad" }'
-        else:
-            self.Answer = b'{ "type" : "Error" , "info" : "User with this name not exist" }'
 
-    def update_user(self, username, new_biometric):
-        if self.AuthSuccess:
-            if self.User == username:
-                self.dbManager.update_user(username, new_biometric)
-                self.Answer = b'{ "type" : "Success" , "info" : "Update user bio_parameters" }'
+        if self.dbManager.exist(username):
+            # Todo Sasha if biometric.auth(bio_parameter,self.dbManager.get_user(name)) :
+            # review : self.dbMan return tuple => using [1] . its bad
+            if self.classifier.compare(
+                    self.PredictPhoto, self.dbManager.get_user(username)[1]):
+
+                self.AuthSuccess = True
+                self.Username = username
+                self.Answer = ServerResponder.success("Auth success")
             else:
-                self.Answer = b'{"type" : "Error" , "info" : " Denied" }'
+                self.Answer = ServerResponder.error("Auth bad")
         else:
-            self.Answer = b'{"type" : "Error" , "info" : "Need Authentication" }'
+            self.Answer = ServerResponder.error("User with this name not exist")
+
+    # Done new_Photo must be vector<256>
+    def update_user(self, username):
+        if self.AuthSuccess:
+            if self.Username == username:
+                self.dbManager.update_user(username, self.PredictPhoto)
+                self.Answer = ServerResponder.success("Update user Photo")
+            else:
+                self.Answer = ServerResponder.error("Denied")
+        else:
+            self.Answer = ServerResponder.error("Need Authentication")
 
     def delete_user(self, username):
         if self.AuthSuccess:
-            if self.User == username:
+            if self.Username == username:
                 self.dbManager.delete_user(username)
-                self.Answer = b'{ "type" : "Success" , "info" : "Delete user" }'
+                self.Answer = ServerResponder.success("Delete user")
                 self.AuthSuccess = False
             else:
-                self.Answer = b'{"type" : "Error" , "info" : " Denied" }'
+                self.Answer = ServerResponder.error("Denied")
         else:
-            self.Answer = b'{"type" : "Error" , "info" : "Need Authentication" }'
+            self.Answer = ServerResponder.error("Need Authentication")
 
     def get_answer(self):
         return self.Answer
 
+    def set_encrypted_session(self, key: bytes):
+        decorator = encryption_sender_decorator_maker(key)
+        self.send, self.recv = decorator(self.send, self.recv)
+
     def processing_request(self, data):
         request = json.loads(data)
-        if request['type'] == 'Registration':
-            self.registration(request['name'], request['biometric'])
+        if request['id'] != self.user_id:
+            self.Answer = ServerResponder.error("Its not you")
 
-        if request['type'] == 'Authentication':
-            self.authentication(request['name'], request['biometric'])
+        if request['type'] == 'HelloACK':
+            self.set_encrypted_session(request['session_key'])
 
-        if request['type'] == 'Update':
-            self.update_user(request['name'], request['new_biometric'])
+        elif request['type'] == 'Registration':
+            if self.download(request['data_size'], request['shape']):
+                self.registration(request['name'])
 
-        if request['type'] == 'Delete':
+        elif request['type'] == 'Authentication':
+            if self.download(request['data_size'], request['shape']):
+                self.authentication(request['name'])
+
+        elif request['type'] == 'Update':
+            if self.download(request['data_size'], request['shape']):
+                self.update_user(request['name'])
+
+        elif request['type'] == 'Delete':
             self.delete_user(request['name'])
+        else:
+            self.Answer = ServerResponder.error("Bad request")
+
+    def download(self, data_size: int, shape: tuple):
+        req = self.recv(data_size)
+        try:
+            pref_photo = np.frombuffer(req, dtype=np.uint8).reshape(shape)
+            # Todo Sasha
+            self.PredictPhoto = self.classifier.predict(pref_photo)
+        except Exception:
+            self.Answer = ServerResponder.error("Bad Photo")
+            return False
+        # Todo change send to success  and fail download
+        self.send(b'Good')
+        return True
+
+
+def start_session(conn: socket.socket, addr: tuple, classifier=None):
+    session = Session(classifier)
+    try:
+        session.start_session(conn, addr)
+    except Exception as e:
+        print(e)
+        print('Session closed')
+        conn.close()
+        return
